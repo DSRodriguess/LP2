5c66cdfa63256f2ea77aed2733a82210fcdb3c70
DSRodriguess <98896245+DSRodriguess@users.noreply.github.com>
2022-02-07 20:59:21 -0300

    mudanças

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -20,15 +20,25 @@ class Hello2DFrame extends JFrame {
         this.setTitle("Java2D - Hello World!");
         this.setSize(350, 350);
         this.setVisible(true);
+
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
+        g2d.setBackground(Color.gray);
+        g2d.setPaint(Color.green);
+        g2d.setStroke(new BasicStroke(2));
         int w = getWidth();
         int h = getHeight();
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
+        g2d.setPaint(Color.MAGENTA);
+        g2d.drawRect(0, 0, 175, 175);
+        g2d.setPaint(Color.red);
+        g2d.fillArc(35, 35, 100, 100, 0, 180);
+        g2d.setPaint(Color.white);
+        g2d.fillArc(35, 35, 100, 100, 180, 180);
+
     }
 }
